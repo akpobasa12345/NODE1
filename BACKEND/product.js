b477ff8b3d7242de89864fd52b34e6dbbc76d875
akpobasa12345 <victorakpobasa2020@gmail.com>
2024-02-01 15:32:18 +0100

    updated

--- BACKEND/product.js
+++ BACKEND/product.js
@@ -1,24 +1,29 @@
-import  Express  from "express";
-import hotel from "./BACKEND/data/osunData.js";
-import lag from "./BACKEND/data/lagosData.js"
+import  express  from "express";
+import hotel from "./data/osunData.js";
+import lag from "./data/lagosData.js"
 import cors from 'cors';
 import cookieParser from "cookie-parser";
 import ibadan from "./data/ibadanData.js";
 import asaba from "./data/asabaData.js";
-import abj from "/data/abujaData.js";
+import abj from "./data/abujaData.js";
 
 
 
 
-const app = Express();
+const app = express();
 app.get("/osunData",(req, res) => {
   res.send({
    data : hotel
   })
-})
-app.get("/lagData",(req, res) => {
+});
 
+app.get("/lagData",(req, res) => {
+  res.send({
+    data : lag
+   })
 })
+
+
 app.use(
   cors({
     origin: "*",
@@ -26,29 +31,29 @@ app.use(
   })
 )
 
-app.use(Express.json())
-app.post((req, res) =>{
-  console.log(req)
-})
-app.post(
-  "/register",
-  (req, res, next) => {
-    // console.log(req);
+// app.use(express.json())
+// app.post((req, res) =>{
+//   console.log(req)
+// })
+// app.post(
+//   "/register",
+//   (req, res, next) => {
+//     // console.log(req);
 
-    const { name, city, state, country } = req.body;
+//     const { name, city, state, country } = req.body;
 
-    if (name == "" || city == "" || state == "" || country == "") {
-    return  res.send("Invalid request");
-    }
+//     if (name == "" || city == "" || state == "" || country == "") {
+//     return  res.send("Invalid request");
+//     }
 
-    next();
-  },
+//     next();
+//   },
 
-  (req, res) => {
-    const { name, city, state, country } = req.body;
-    res.send({ name, city, state });
-  }
-);
+//   (req, res) => {
+//     const { name, city, state, country } = req.body;
+//     res.send({ name, city, state });
+//   }
+// );
 
 app.listen(8080, () => {
   console.log("work")
